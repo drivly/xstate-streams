37126f80f0bbeb1f392c4e2a6ad2ead5390b9e7b
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-11-07 08:58:38 -0600

    Updates to tests

---
+++ mongo/tests.ts
@@ -0,0 +1,9 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+// This will create an new instance of "MongoMemoryServer" and automatically start it
+// const mongod = await MongoMemoryServer.create();
+
+// const uri = mongod.getUri();
+
+// The Server can be stopped again with
+// await mongod.stop();
